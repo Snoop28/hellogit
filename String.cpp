db72fee174173e753197cfb36e92c6899b7694e0
Snoop28 <Snoop28@users.noreply.github.com>
2019-02-02 00:28:42 -0400

    Added source file

---
+++ String.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+ string car="";
+ 
+ cout << "Please enter a car manufacturer" << endl;
+ cin >> car;
+ 
+ cout << "Your choice of manufacturer is" +car << endl;
+ 
+ return 0;
+}
